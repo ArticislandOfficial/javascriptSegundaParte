1e6c77bb3c045ed0d1f4b66f6dfa6bb41e4a93b5
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-16 21:26:11 -0500

    terminando POO y agregando DOM

--- rectangulo.js
+++ rectangulo.js
@@ -1,12 +1,16 @@
 //Ejmeplo POO
 
 class Rectangulo {
-  base = 0;
-  altura = 0;
+  // base = 0;
+  // altura = 0;
 
-  constructor() {
+  constructor(base,altura) {
+    this.base=base;
+    this.altura=altura;
+    // this.area()
     console.log('desde el metodo constructor');
   }
+  //Metodos 
   area() {
     return this.base * this.altura;
   }
@@ -15,9 +19,12 @@ class Rectangulo {
   }
 }
 
-let rectanguloUno = new Rectangulo();
-rectanguloUno.altura = 5;
-rectanguloUno.base = 5;
+let rectanguloUno = new Rectangulo(5,5);
+// rectanguloUno.altura = 5;
+// rectanguloUno.base = 5;
 
-console.log(rectanguloUno.area());
-console.log(rectanguloUno.perimetro());
+ console.log(rectanguloUno.area());
+ console.log(rectanguloUno.perimetro());
+///DOM
+
+document.getElementById("respuesta").innerHTML = rectanguloUno.area();
