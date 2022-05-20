aff2fd8a607d99f3a50917ce71a103fd517353dc
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-19 23:52:10 -0500

    Segundo ejercicio de clase ..

---
+++ tema_01_DOM/ejercicios_02_DOM/app.js
@@ -0,0 +1,14 @@
+const eventoOne = ()=>{
+    let cambioColor = document.querySelector('.divOne');
+   cambioColor.style.backgroundColor = "red";
+}
+const eventoTwo = () => {
+  let cambioColor = document.querySelector(".divTwo");
+  cambioColor.style.height = "100px";
+  cambioColor.style.width = "200px";
+
+};
+const eventoThree = () => {
+  let cambioColor = document.querySelector(".divThree");
+  cambioColor.style.borderRadius = "30px";
+};
