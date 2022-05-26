db9957c91392a11d30dc0e3c6e06524f353b363e
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-25 23:15:49 -0500

    tablas

--- Reto de la semana/tablas de multiplicar/tablas.js
+++ Reto de la semana/tablas de multiplicar/tablas.js
@@ -1,12 +1,18 @@
 let saludo = () => {
-  console.log("Hola a todos");
+  // console.log("Hola a todos");
+  let tablas = document.getElementsByTagName("table");
+  for (let tabla = 0; tabla < tablas.length; tabla++) {
+    //console.log(tablas.length);
+    for (let ciclo = 1; ciclo <= 11; ciclo++) {
+      let texth1 = document.createElement("tr");
+      let resultado = ciclo * tabla;
+      texth1.innerHTML = `<td> ${tabla} x ${ciclo} </td><td>=${resultado} </td> `;
+      // Arreglo
+      document.getElementsByTagName("tbody")[tabla].appendChild(texth1);
+    }
+  }
 };
-
-for (let index = 0; index < array.length; index++) {
-    console.log(index);
-    const element = array[index];
-    console.log(element );
-}
+saludo();
 
 // let onmouseoutTest =() =>{
 //     console.log("Disparo onmouseout");
